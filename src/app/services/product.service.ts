5b14a8a96bdc719f0d238d6281946a1138e0309a
mw <martinez2@gmx.de>
2022-06-08 21:06:01 +0200

    feat: Add getProducts method to ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../models/product';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class ProductService {
-	constructor() {}
+	constructor(private http: HttpClient) {}
+
+	/**
+	 * @description Get the products from the backend.
+	 * @returns {Observable<Product[]>} - The stream of fetched products
+	 */
+	getProducts(): Observable<Product[]> {
+		// Fetch data
+		const observable = this.http.get<Product[]>('../../assets/data.json');
+
+		// Set amount to 1
+		return observable.pipe<Product[]>(
+			map((products) =>
+				products.map((product) => {
+					product.amount = 1;
+					return product;
+				})
+			)
+		);
+	}
 }
