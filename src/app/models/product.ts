a80bd537586953204f922cb6bf5ea153e2847621
mw <martinez2@gmx.de>
2022-06-08 14:01:03 +0200

    feat: Add model Product

---
+++ src/app/models/product.ts
@@ -0,0 +1,12 @@
+/**
+ * @description Contains information about a product
+ * and its ordered quantity.
+ */
+export type Product = {
+	id: number;
+	name: string;
+	price: number;
+	url: string;
+	description: string;
+	amount: number;
+};
