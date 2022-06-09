f062c29f5aaff7605526d5a0fae1e437d2572a54
mw <martinez2@gmx.de>
2022-06-09 11:00:03 +0200

    feat: Add FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { ProductItemComponent } from './components/product-item/product-item.com
 
 @NgModule({
 	declarations: [AppComponent, ProductListComponent, ProductItemComponent],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
