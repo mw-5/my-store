a8bfa0f57599a8c397d38b411002be79d6f3f9c2
mw <martinez2@gmx.de>
2022-06-10 22:09:25 +0200

    feat: Add cart-item to parent cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -15,4 +15,25 @@ export class CartComponent implements OnInit {
 	ngOnInit(): void {
 		this.cart = this.cartService.getProducts();
 	}
+
+	/**
+	 * @description Handle new amount of product.
+	 * @param {Product} product - The product to be updated
+	 * @param {number} newAmount - The new amount
+	 */
+	onAmountUpdated(product: Product, newAmount: number): void {
+		// Find position of product in cart
+		const index = this.cart.findIndex(
+			(p) => p.id == product.id && p.amount == product.amount
+		);
+		if (index != -1) {
+			if (newAmount == 0) {
+				// Remove product from cart
+				this.cart.splice(index, 1);
+			} else {
+				// Update amount in cart
+				this.cart[index].amount = newAmount;
+			}
+		}
+	}
 }
