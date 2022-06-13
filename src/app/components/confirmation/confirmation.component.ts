72ce7dd7565ae851f083502b1721203900321970
mw <martinez2@gmx.de>
2022-06-13 21:44:49 +0200

    feat: Add go-back button to confirmation

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-confirmation',
@@ -11,7 +12,11 @@ export class ConfirmationComponent implements OnInit {
 	username = '';
 	orderTotal = 0;
 
-	constructor(private cart: CartService, private userService: UserService) {}
+	constructor(
+		private cart: CartService,
+		private userService: UserService,
+		private router: Router
+	) {}
 
 	ngOnInit(): void {
 		// Set properties
@@ -22,5 +27,12 @@ export class ConfirmationComponent implements OnInit {
 		// Reset services
 		this.cart.resetCart();
 		this.userService.resetUser();
+	}
+
+	/**
+	 * @description Go to product list.
+	 */
+	goToProductList(): void {
+		this.router.navigate(['/']);
 	}
 }
